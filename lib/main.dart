133f2030cc2dc93e566a4d6388c24e229101549f
Zacharias02 <necesitolester@gmail.com>
2020-09-17 14:20:10 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Sneakerbox App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomLeft,
+            colors: [
+              Colors.orange[900],
+              Colors.orange[800],
+              Colors.orange[400],
+            ],
+          ),
+          image: DecorationImage(
+            image: AssetImage('assets/images/aj7_cr/jcr-a.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
